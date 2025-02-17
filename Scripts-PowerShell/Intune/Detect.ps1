29965672d5e9ed7922dc8677b14747ef427313da
Diego Rodrigues <85366337+Yurtachi@users.noreply.github.com>
2025-02-17 13:53:58 -0300

    Add Intune detection and remediation scripts
    Adicionado scripts PowerShell para o Intune:
    - detect.ps1: Verifica se o usuário 'support' está ativado e retorna código 0 (ativo) ou 1 (inativo).
    - remediate.ps1: Ativa o usuário 'support' caso esteja desativado.

---
+++ Scripts-PowerShell/Intune/Detect.ps1
@@ -0,0 +1,12 @@
+﻿# Verifica se o usuário "support" está ativado
+$user = Get-LocalUser -Name "support"
+
+# Se o usuário estiver ativado, retorna código 0 
+if ($user.Enabled) {
+    Write-Host ('Ola')
+    exit 0
+} else {
+    # Caso contrário, retorna código 1 
+    Write-Host ('Ola')
+    exit 1
+}
---
+++ Scripts-PowerShell/Intune/Remediate.ps1
@@ -0,0 +1,17 @@
+﻿# Ativa o usuário "support" caso esteja desativado
+$user = Get-LocalUser -Name "support"
+
+if (-not $user.Enabled) {
+    $user | Enable-LocalUser
+}
+
+# Verifica se a remediação foi bem-sucedida
+$user = Get-LocalUser -Name "support"
+
+# Se o usuário estiver ativado, retorna código 0 (sucesso)
+if ($user.Enabled) {
+    exit 0
+} else {
+    # Caso contrário, retorna código 1 (falha)
+    exit 1
+}
