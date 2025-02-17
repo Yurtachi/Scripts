78bda18f742b3dbc6986134cbb74e548b06cf21f
Diego Rodrigues <85366337+Yurtachi@users.noreply.github.com>
2025-02-17 09:27:27 -0300

    Add Manage-WindowsActivation.ps1 script for OEM key management
    Adicionado o script 'Manage-WindowsOEMKey.ps1' para realizar o processo de obtenção da chave OEM armazenada no firmware, remoção de chaves existentes, instalação de uma nova chave de produto e ativação do Windows.

---
+++ Scripts-PowerShell/Manage-WindowsActivation.ps1
@@ -0,0 +1,48 @@
+﻿# Função para obter a chave de produto OEM do firmware
+function Get-OEMKey {
+    try {
+        $oemKey = (Get-WmiObject -Query "SELECT * FROM SoftwareLicensingService").OA3xOriginalProductKey
+        return $oemKey
+    } catch {
+        return $null
+    }
+}
+
+# Função para desinstalar qualquer chave existente
+function Remove-CurrentKey {
+    try {
+        Start-Process -FilePath "cscript.exe" -ArgumentList "//NoLogo", "C:\Windows\System32\slmgr.vbs", "/upk" -NoNewWindow -Wait
+    } catch {
+        # Nothing
+    }
+}
+
+# Função para instalar uma nova chave de produto
+function Install-Key {
+    param (
+        [string]$Key
+    )
+    try {
+        Start-Process -FilePath "cscript.exe" -ArgumentList "//NoLogo", "C:\Windows\System32\slmgr.vbs", "/ipk", $Key -NoNewWindow -Wait
+    } catch {
+        # # Nothing
+    }
+}
+
+# Função para ativar o Windows
+function Activate-Windows {
+    try {
+        Start-Process -FilePath "cscript.exe" -ArgumentList "//NoLogo", "C:\Windows\System32\slmgr.vbs", "/ato" -NoNewWindow -Wait
+    } catch {
+        # Nothing
+    }
+}
+
+# Fluxo principal
+$oemKey = Get-OEMKey
+
+if ($oemKey) {
+    Remove-CurrentKey
+    Install-Key -Key $oemKey
+    Activate-Windows
+}
